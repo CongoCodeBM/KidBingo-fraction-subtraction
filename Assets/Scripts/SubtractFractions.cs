f68d2d9618eae47342ada497fffc568347805099
CongoCodeBM <bnmunganga.bnm@gmail.com>
2022-10-11 03:34:24 -0700

    Changes to script

--- Assets/Scripts/SubtractFractions.cs
+++ Assets/Scripts/SubtractFractions.cs
@@ -3,8 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Numerics;
+using System.Linq;
 using Fractions;
-using UnityEngine.UIElements;
+using UnityEngine.EventSystems;
+
+//This is an extension class that helps us create an extension method that allows us to shuffle a list 
+public static class Extensions
+{
+    private static System.Random randomizer = new System.Random();
+
+    public static void Shuffle<T>(this IList<T> values)
+    {
+        for (int i = values.Count - 1; i > 0; i--)
+        {
+            int k = randomizer.Next(i + 1);
+            T value = values[k];
+            values[k] = values[i];
+            values[i] = value;
+        }
+    }
+}
+
 
 public class SubtractFractions : MonoBehaviour
 {
@@ -71,13 +90,72 @@ public class SubtractFractions : MonoBehaviour
             ResultList.Add(ResultFraction);
         }
 
-       
+        ResultList.Shuffle(); //shuffling the list that will be used to display the answers
     }    
+
+
 
     // Start is called before the first frame update
    void Start()
    {
         AddElementsToTheirStacks();
+        ShowQuestionAndAnswerOptionsToScreen();
+        Button btn1 = button1.GetComponent<Button>();
+        btn1.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn2 = button2.GetComponent<Button>();
+        btn2.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn3 = button3.GetComponent<Button>();
+        btn3.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn4 = button4.GetComponent<Button>();
+        btn4.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn5 = button5.GetComponent<Button>();
+        btn5.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn6 = button6.GetComponent<Button>();
+        btn6.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn7 = button7.GetComponent<Button>();
+        btn7.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn8 = button8.GetComponent<Button>();
+        btn8.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn9 = button9.GetComponent<Button>();
+        btn9.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn10 = button10.GetComponent<Button>();
+        btn10.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn11 = button11.GetComponent<Button>();
+        btn11.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn12 = button12.GetComponent<Button>();
+        btn12.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn13 = button13.GetComponent<Button>();
+        btn13.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn14 = button14.GetComponent<Button>();
+        btn14.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn15 = button15.GetComponent<Button>();
+        btn15.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn16 = button16.GetComponent<Button>();
+        btn16.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn17 = button17.GetComponent<Button>();
+        btn17.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn18 = button18.GetComponent<Button>();
+        btn18.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn19 = button19.GetComponent<Button>();
+        btn19.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn20 = button20.GetComponent<Button>();
+        btn20.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn21 = button21.GetComponent<Button>();
+        btn21.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn22 = button22.GetComponent<Button>();
+        btn22.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn23 = button23.GetComponent<Button>();
+        btn23.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn24 = button24.GetComponent<Button>();
+        btn24.onClick.AddListener(ToDoWhenButtonClicked);
+        Button btn25 = button25.GetComponent<Button>();
+        btn25.onClick.AddListener(ToDoWhenButtonClicked);
+
+    }
+
+    //This method is used to populate the UI with responses on buttons, and questions on the specified text area
+    void ShowQuestionAndAnswerOptionsToScreen()
+    {
         questionTextObject = questionObject.GetComponent<Text>();
         textObject1 = buttonObject1.GetComponent<Text>();
         textObject2 = buttonObject2.GetComponent<Text>();
@@ -106,31 +184,31 @@ public class SubtractFractions : MonoBehaviour
         textObject25 = buttonObject25.GetComponent<Text>();
 
         //Display the answer options
-        textObject1.text = ResultStack.Pop().ToString();
-        textObject2.text = ResultStack.Pop().ToString();
-        textObject3.text = ResultStack.Pop().ToString();
-        textObject4.text = ResultStack.Pop().ToString();
-        textObject5.text = ResultStack.Pop().ToString();
-        textObject6.text = ResultStack.Pop().ToString();
-        textObject7.text = ResultStack.Pop().ToString();
-        textObject8.text = ResultStack.Pop().ToString();
-        textObject9.text = ResultStack.Pop().ToString();
-        textObject10.text = ResultStack.Pop().ToString();
-        textObject11.text = ResultStack.Pop().ToString();
-        textObject12.text = ResultStack.Pop().ToString();
-        textObject13.text = ResultStack.Pop().ToString();
-        textObject14.text = ResultStack.Pop().ToString();
-        textObject15.text = ResultStack.Pop().ToString();
-        textObject16.text = ResultStack.Pop().ToString();
-        textObject17.text = ResultStack.Pop().ToString();
-        textObject18.text = ResultStack.Pop().ToString();
-        textObject19.text = ResultStack.Pop().ToString();
-        textObject20.text = ResultStack.Pop().ToString();
-        textObject21.text = ResultStack.Pop().ToString();
-        textObject22.text = ResultStack.Pop().ToString();
-        textObject23.text = ResultStack.Pop().ToString();
-        textObject24.text = ResultStack.Pop().ToString();
-        textObject25.text = ResultStack.Pop().ToString();
+        textObject1.text = ResultList.ElementAt(0).ToString(); //ResultStack.Pop().ToString();
+        textObject2.text = ResultList.ElementAt(1).ToString(); //ResultStack.Pop().ToString();
+        textObject3.text = ResultList.ElementAt(2).ToString(); //ResultStack.Pop().ToString();
+        textObject4.text = ResultList.ElementAt(3).ToString(); //ResultStack.Pop().ToString();
+        textObject5.text = ResultList.ElementAt(4).ToString(); //ResultStack.Pop().ToString();
+        textObject6.text = ResultList.ElementAt(5).ToString(); //ResultStack.Pop().ToString();
+        textObject7.text = ResultList.ElementAt(6).ToString(); //ResultStack.Pop().ToString();
+        textObject8.text = ResultList.ElementAt(7).ToString(); //ResultStack.Pop().ToString();
+        textObject9.text = ResultList.ElementAt(8).ToString(); //ResultStack.Pop().ToString();
+        textObject10.text = ResultList.ElementAt(9).ToString(); //ResultStack.Pop().ToString();
+        textObject11.text = ResultList.ElementAt(10).ToString(); //ResultStack.Pop().ToString();
+        textObject12.text = ResultList.ElementAt(11).ToString(); //ResultStack.Pop().ToString();
+        textObject13.text = ResultList.ElementAt(12).ToString(); //ResultStack.Pop().ToString();
+        textObject14.text = ResultList.ElementAt(13).ToString(); //ResultStack.Pop().ToString();
+        textObject15.text = ResultList.ElementAt(14).ToString(); //ResultStack.Pop().ToString();
+        textObject16.text = ResultList.ElementAt(15).ToString(); //ResultStack.Pop().ToString();
+        textObject17.text = ResultList.ElementAt(16).ToString(); //ResultStack.Pop().ToString();
+        textObject18.text = ResultList.ElementAt(17).ToString(); //ResultStack.Pop().ToString();
+        textObject19.text = ResultList.ElementAt(18).ToString(); //ResultStack.Pop().ToString();
+        textObject20.text = ResultList.ElementAt(19).ToString(); //ResultStack.Pop().ToString();
+        textObject21.text = ResultList.ElementAt(20).ToString(); //ResultStack.Pop().ToString();
+        textObject22.text = ResultList.ElementAt(21).ToString(); //ResultStack.Pop().ToString();
+        textObject23.text = ResultList.ElementAt(22).ToString(); //ResultStack.Pop().ToString();
+        textObject24.text = ResultList.ElementAt(23).ToString(); //ResultStack.Pop().ToString();
+        textObject25.text = ResultList.ElementAt(24).ToString(); //ResultStack.Pop().ToString();
 
         //Display the question. If the the fraction on the left is less than the fraction on the right, we swap their position on the display
         var c = System.Convert.ToDecimal(CheckGreaterFractionStack.Peek().ToString());
@@ -138,16 +216,99 @@ public class SubtractFractions : MonoBehaviour
         //if (System.Convert.ToDecimal(CheckGreaterFractionStack.Peek().ToString()) < 0)
         if (c < 0) //questionTextObject.text = "Fraction A - Fraction B = \n" + c;
         {
-            questionTextObject.text = FractionBStack.Pop().ToString() + " - "
-                               + FractionAStack.Pop().ToString() + "\n=? "; 
+            questionTextObject.text = FractionBStack.Peek().ToString() + " - "
+                               + FractionAStack.Peek().ToString() + "\n=? ";
         }
         else //questionTextObject.text = "Fraction B - Fraction A = \n" + c;
-            questionTextObject.text = FractionAStack.Pop().ToString() + " - "
-                              + FractionBStack.Pop().ToString() + "\n= ?";
-
+            questionTextObject.text = FractionAStack.Peek().ToString() + " - "
+                              + FractionBStack.Peek().ToString() + "\n= ?";
     }
 
 
+    void ToDoWhenButtonClicked()
+    {
+        var c = System.Convert.ToDecimal(CheckGreaterFractionStack.Peek().ToString());
+        var textOnCurrentButton = GameObject.Find(EventSystem.current.currentSelectedGameObject.name).GetComponent<Button>().GetComponentInChildren<Text>().text;
+
+        if (c < 0)
+        {
+            Debug.Log("ResultStack.Pop().ToString() : " + ResultStack.Peek().ToString());
+            Debug.Log("textOnCurrentButton.ToString() : " + textOnCurrentButton.ToString());
+
+            if (ResultStack.Pop().ToString().Equals(textOnCurrentButton.ToString()))
+            {
+                FractionAStack.Pop();
+                FractionBStack.Pop();
+                CheckGreaterFractionStack.Pop();
+                c = System.Convert.ToDecimal(CheckGreaterFractionStack.Peek().ToString());
+
+                //Change display image of button to lamb image on top of button
+
+                //Add 1 to success count
+
+                if (c < 0) questionTextObject.text = FractionBStack.Peek().ToString() + " - "
+                            + FractionAStack.Peek().ToString() + "\n= ? ";
+                else questionTextObject.text = FractionAStack.Peek().ToString() + " - "
+                            + FractionBStack.Peek().ToString() + "\n= ? ";
+            }
+
+            else
+            {
+                FractionAStack.Pop();
+                FractionBStack.Pop();
+                CheckGreaterFractionStack.Pop();
+                c = System.Convert.ToDecimal(CheckGreaterFractionStack.Peek().ToString());
+
+                //show next question
+                if (c < 0) questionTextObject.text = FractionBStack.Peek().ToString() + " - "
+                                   + FractionAStack.Peek().ToString() + "\n=? ";
+                else questionTextObject.text = FractionAStack.Peek().ToString() + " - "
+                            + FractionBStack.Peek().ToString() + "\n= ? ";
+
+                //destroy button and make a sound
+                EventSystem.current.currentSelectedGameObject.SetActive(false);
+            }
+                
+        }
+
+        else
+        {
+            if (ResultStack.Pop().ToString().Equals(textOnCurrentButton.ToString()))
+            {
+                FractionAStack.Pop();
+                FractionBStack.Pop();
+                CheckGreaterFractionStack.Pop();
+                c = System.Convert.ToDecimal(CheckGreaterFractionStack.Peek().ToString());
+
+                //Change display image of button to lamb image on top of button
+
+                //Add 1 to success count
+
+                //display next question
+                if (c >= 0) questionTextObject.text = FractionAStack.Peek().ToString() + " - "
+                                  + FractionBStack.Peek().ToString() + "\n= ?";
+                else questionTextObject.text = FractionBStack.Peek().ToString() + " - "
+                                  + FractionAStack.Peek().ToString() + "\n= ?";
+            }
+
+            else
+            {
+                FractionAStack.Pop();
+                FractionBStack.Pop();
+                CheckGreaterFractionStack.Pop();
+                c = System.Convert.ToDecimal(CheckGreaterFractionStack.Peek().ToString());
+
+                //destroy button and make a sound
+                EventSystem.current.currentSelectedGameObject.SetActive(false);
+
+                //display next question
+                if (c >= 0) questionTextObject.text = FractionAStack.Peek().ToString() + " - "
+                                  + FractionBStack.Peek().ToString() + "\n= ?";
+                else questionTextObject.text = FractionBStack.Peek().ToString() + " - "
+                                  + FractionAStack.Peek().ToString() + "\n= ?";
+            }
+        }
+    }
 
     // Update is called once per frame
     void Update()
